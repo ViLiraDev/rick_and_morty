7228fa8779c8a05facf6fab9f784391001847ac5
unknown <vilira2501@gmail.com>
2023-04-13 17:47:39 -0300

    Fixed Favs and GitHub Logo

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,24 +1,23 @@
 import styles from '../Card/card.module.css'
 import { Link } from 'react-router-dom';
 import React, { useRef } from 'react';
-import { useDispatch } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { addFav, removeFav } from '../../redux/actions';
 import { connect } from 'react-redux';
 
 
-function Card({ id,name, species, gender, onClose, image,addFav, removeFav, myFavorites }) {
-
+function Card({ id, name, species, gender, onClose, image, addFav, removeFav, myFavorites }) {
    const [isFav, setIsFav] = useState(false);
+   const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
    const handleFavorite = () => {
-      if(isFav){
+      if (isFav) {
          setIsFav(false);
          removeFav(id);
       }
       else {
          setIsFav(true);
-         addFav({id, name, species, gender, image, onClose})
+         addFav({ id, name, species, gender, image, onClose })
       }
    }
 
@@ -32,21 +31,27 @@ function Card({ id,name, species, gender, onClose, image,addFav, removeFav, myFa
 
    const audioRef = useRef(null);
    function handleMouseEnter() {
-      
+
       audioRef.current.currentTime = 0;
       audioRef.current.play();
-    }
+   }
 
+   useEffect(() => {
+      setCurrentPath(window.location.pathname);
+   }, [window.location.pathname]);
 
    return (
       <div className={styles.cardContainer} onMouseEnter={handleMouseEnter}>
-         
+
          <div className={styles.card}>
             <div className={styles.btn}>
-               <button className={styles.btnFav} onClick={handleFavorite}>{isFav ? '❤️' : '🤍' }</button>
-               <button className={styles.btnDel} onClick={() => onClose(id)}>X</button>
+               <button className={styles.btnFav} onClick={handleFavorite}>{isFav ? '❤️' : '🤍'}</button>
+               {currentPath !== '/favorites' && (
+               <button className={styles.btnDel} onClick={() => onClose(id)}>X</button>   
+               )}
+               
             </div>
-            <Link to={`/detail/${id}`} style={{textDecoration:'none',color:'#227093'}}>
+            <Link to={`/detail/${id}`} style={{ textDecoration: 'none', color: '#227093' }}>
                <h2 className={styles.cardName}>{name}</h2>
             </Link>
             <h2>{species}</h2>
@@ -56,10 +61,11 @@ function Card({ id,name, species, gender, onClose, image,addFav, removeFav, myFa
                <source src={process.env.PUBLIC_URL + '/burbuja.mp3'} type="audio/mpeg" />
             </audio>
 
-         </div> 
+         </div>
       </div>
    );
 }
+
 const mapStateToProps = (state) => {
    return {
       myFavorites: state.myFavorites
